2400b5f23dca1e76bd9d5524a5e17a94730be7db
THATSADAPHONE ITPD <dayeriveren@outlook.com>
2021-10-04 16:15:45 +0700

    Edit and use external index.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+//import Heander from components
+import Header from "./components/Header";
+//import React of react components
+import React from 'react'
 
+//this Belong to Header
 function App() {
+  const name = "Bee"
+  const x = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <h1 style={{color: 'red'}}>Hello Form React</h1>
+      <h2>Hello FUll ? {name} </h2>
+      <h2>Are you okie? {x? 'Yes': 'No'}</h2>
+      <br></br>
+      <Header />
+    </div>  
   );
 }
+
+//other function of class 
+//use class and call name App for using reder
+
+// class App extends React.Component{
+//   render(){
+//     return <h1>Hello Class App React</h1>
+//   }
+// }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types'
+
+//use for class and function
+//use 'rfce' to impore fnction header below
+//to pass in anything in HeaderofApp.js
+//use prop in components as parameter
+//props is obj
+
+// const Header = (props) => {
+//     return (
+//         <header>
+//             <h1>Hello Task Tracker</h1>
+//             <h1>{props.title}</h1>
+//         </header>
+        
+//     )
+// }
+
+//can change both way (props) or ({titlej})
+const Header = ({title}) => {
+    return (
+        <header>
+            <h1 style= {{color: 'green', backgroundColor: 'black'}}>Hello Task Tracker with inline sytle</h1>
+            <h1 style={HeaderSytle}>{title}</h1>
+        </header>    
+    )
+}
+//we can .defaultProps with use title=""in App
+//it wil overide it
+Header.defaultProps = {
+    title: 'Default Header Task',
+}
+//use propType to create type of str number or boolen
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+    //title: PropTypes.number,
+}
+// not create sytle css inline
+// let create variable outline
+const HeaderSytle = {
+    color: 'orange', 
+    backgroundColor: 'black'
+}
+export default Header
